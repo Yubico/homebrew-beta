11b2dc71b6424d47441d807b2c77603f44b9d683
Dag Heyman <dag@yubico.com>
2020-05-12 08:47:22 +0200

    yubioath-desktop 5.0.4-beta1

--- Casks/yubioath-desktop-beta.rb
+++ Casks/yubioath-desktop-beta.rb
@@ -1,7 +1,7 @@
 cask 'yubioath-desktop-beta' do
-  version '5.0.3-beta2'
-  sha256 '53f3240292f37432c5df60d8085261e64fb0359471b02e233b2f4e0ced38738f'
-  url "https://github.com/Yubico/yubioath-desktop/releases/download/yubioath-desktop-5.0.3-beta2/yubioath-desktop-5.0.3-beta2-mac.pkg"
+  version '5.0.4-beta1'
+  sha256 'f4b1047cf24309bb7df9e5736d4ce303f33f02a8a950307f29dcf2219def7d4d'
+  url 'https://github.com/Yubico/yubioath-desktop/releases/download/yubioath-desktop-5.0.4-beta1/yubioath-desktop-5.0.4-beta1-mac.pkg'
   name 'Yubico Authenticator (Beta)'
   homepage 'https://developers.yubico.com/yubioath-desktop/'
   depends_on macos: '>= :high_sierra'
